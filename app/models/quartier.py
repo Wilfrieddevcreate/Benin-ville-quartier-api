7f46bfaf44fa2a3e0bd26a854b462ce2f3ceeb94
Wilfrieddevcreate <yves.heloussato@epitech.eu>
2025-03-10 16:56:57 +0100

    File procfile

--- app/models/quartier.py
+++ app/models/quartier.py
@@ -1,5 +1,116 @@
 quartiers = [
+    # Cotonou
     {"id": 1, "nom": "Zongo", "ville_id": 1},
     {"id": 2, "nom": "Fidjrossè", "ville_id": 1},
-    {"id": 3, "nom": "Tokpota", "ville_id": 2},
-]   
+    {"id": 3, "nom": "Akpakpa", "ville_id": 1},
+    {"id": 4, "nom": "Jéricho", "ville_id": 1},
+    {"id": 5, "nom": "Cadjèhoun", "ville_id": 1},
+    {"id": 6, "nom": "Ste Rita", "ville_id": 1},
+    {"id": 7, "nom": "Agla", "ville_id": 1},
+    {"id": 8, "nom": "Gbégamey", "ville_id": 1},
+    
+    # Porto-Novo
+    {"id": 9, "nom": "Tokpota", "ville_id": 2},
+    {"id": 10, "nom": "Dowa", "ville_id": 2},
+    {"id": 11, "nom": "Avakpa", "ville_id": 2},
+    {"id": 12, "nom": "Adjina", "ville_id": 2},
+    {"id": 13, "nom": "Catchi", "ville_id": 2},
+
+    # Parakou
+    {"id": 14, "nom": "Zongo", "ville_id": 3},
+    {"id": 15, "nom": "Albarika", "ville_id": 3},
+    {"id": 16, "nom": "Titirou", "ville_id": 3},
+    {"id": 17, "nom": "Wansirou", "ville_id": 3},
+    {"id": 18, "nom": "Banikanni", "ville_id": 3},
+
+    # Abomey
+    {"id": 19, "nom": "Djidja", "ville_id": 4},
+    {"id": 20, "nom": "Hounli", "ville_id": 4},
+    {"id": 21, "nom": "Agbokpa", "ville_id": 4},
+
+    # Bohicon
+    {"id": 22, "nom": "Gnidjazoun", "ville_id": 5},
+    {"id": 23, "nom": "Sodo", "ville_id": 5},
+    {"id": 24, "nom": "Houto", "ville_id": 5},
+
+    # Natitingou
+    {"id": 25, "nom": "Kouandata", "ville_id": 6},
+    {"id": 26, "nom": "Tigninti", "ville_id": 6},
+    {"id": 27, "nom": "Koussoucoingou", "ville_id": 6},
+
+    # Djougou
+    {"id": 28, "nom": "Pabegou", "ville_id": 7},
+    {"id": 29, "nom": "Onklou", "ville_id": 7},
+
+    # Lokossa
+    {"id": 30, "nom": "Agamé", "ville_id": 8},
+    {"id": 31, "nom": "Gbakpodji", "ville_id": 8},
+
+    # Ouidah
+    {"id": 32, "nom": "Pahou", "ville_id": 9},
+    {"id": 33, "nom": "Djègbadji", "ville_id": 9},
+
+    # Kandi
+    {"id": 34, "nom": "Gomparou", "ville_id": 10},
+    {"id": 35, "nom": "Angaradébou", "ville_id": 10},
+
+    # Malanville
+    {"id": 36, "nom": "Garou", "ville_id": 11},
+    {"id": 37, "nom": "Malanville Centre", "ville_id": 11},
+
+    # Savalou
+    {"id": 38, "nom": "Tchetti", "ville_id": 12},
+    {"id": 39, "nom": "Gbanlin", "ville_id": 12},
+
+    # Sakété
+    {"id": 40, "nom": "Ita-Djèbou", "ville_id": 13},
+    {"id": 41, "nom": "Takon", "ville_id": 13},
+
+    # Allada
+    {"id": 42, "nom": "Ahomey", "ville_id": 14},
+    {"id": 43, "nom": "Hinvi", "ville_id": 14},
+
+    # Aplahoué
+    {"id": 44, "nom": "Azovè", "ville_id": 15},
+    {"id": 45, "nom": "Gohomey", "ville_id": 15},
+
+    # Comé
+    {"id": 46, "nom": "Agbanto", "ville_id": 16},
+    {"id": 47, "nom": "Kpinnou", "ville_id": 16},
+
+    # Bembéréké
+    {"id": 48, "nom": "Sina-Kounou", "ville_id": 17},
+    {"id": 49, "nom": "Guinagourou", "ville_id": 17},
+
+    # Bassila
+    {"id": 50, "nom": "Manigri", "ville_id": 18},
+    {"id": 51, "nom": "Pénéssoulou", "ville_id": 18},
+
+    # Tchaourou
+    {"id": 52, "nom": "Sanson", "ville_id": 19},
+    {"id": 53, "nom": "Goro", "ville_id": 19},
+
+    # Pobè
+    {"id": 54, "nom": "Issaba", "ville_id": 20},
+    {"id": 55, "nom": "Ahoyéyé", "ville_id": 20},
+
+    # Kérou
+    {"id": 56, "nom": "Brignamaro", "ville_id": 21},
+    {"id": 57, "nom": "Kaobagou", "ville_id": 21},
+
+    # Cové
+    {"id": 58, "nom": "Houin", "ville_id": 22},
+    {"id": 59, "nom": "Zounguê", "ville_id": 22},
+
+    # Nikki
+    {"id": 60, "nom": "Sérékalé", "ville_id": 23},
+    {"id": 61, "nom": "Biro", "ville_id": 23},
+
+    # Banikoara
+    {"id": 62, "nom": "Gomparou", "ville_id": 24},
+    {"id": 63, "nom": "Somperou", "ville_id": 24},
+
+    # Tanguiéta
+    {"id": 64, "nom": "Tanéka", "ville_id": 25},
+    {"id": 65, "nom": "Toukountouna", "ville_id": 25},
+]
